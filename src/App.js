76f206050ff356adc33f338d4eb7730d638b1a23
folkert89 <folkertburghout@hotmail.com>
2018-04-01 14:35:18 +0200

    setup react and redux

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Game Ohh1</h1>
+          <h1 className="title">0hh1</h1>
         </header>
         <p className="App-intro">
           To get started, click here for the rules
